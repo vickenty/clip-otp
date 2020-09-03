1fe07a1106aeb444a33b877b897d739a06363f8a
Vickenty Fesunov <kent@setattr.net>
2020-09-03 23:36:27 +0200

    Drop gtk support
    X11 provides more information about requestor and even allows to reject clipboard requests

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
 use anyhow::Result;
 
-#[cfg(feature = "with_gtk")]
-mod gtk;
-
 #[cfg(feature = "with_x11")]
 mod x11;
 
 fn main() -> Result<()> {
-    #[cfg(feature = "with_gtk")]
-    if std::env::var("XDG_CURRENT_DESKTOP")
-        .as_ref()
-        .map(|x| &x[..])
-        == Ok("GNOME")
-    {
-        gtk::gtk()?;
-        return Ok(());
-    }
-
     #[cfg(feature = "with_x11")]
     if std::env::var_os("DISPLAY").is_some() {
         x11::x11()?;
