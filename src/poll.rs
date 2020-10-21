d5044cef757ddabd0bf2005c59a00466f0e9c0ed
Vickenty Fesunov <kent@setattr.net>
2020-10-21 21:42:56 +0200

    Replace slice with slice::from_mut
    Shorter code and no .unwrap()

--- src/poll.rs
+++ src/poll.rs
@@ -1,24 +1,18 @@
 use libc;
-use std::convert::TryInto;
 use std::io::Result;
 use std::os::unix::io::AsRawFd;
+use std::slice::from_mut;
 
 pub fn wait_with_timeout<T: AsRawFd>(fd: &T, timeout_ms: i32) -> Result<()> {
     let fd = fd.as_raw_fd();
 
-    let pollfd = &mut [libc::pollfd {
+    let pollfd = &mut libc::pollfd {
         fd,
         events: libc::POLLIN,
         revents: 0,
-    }][..];
-
-    let rc = unsafe {
-        libc::poll(
-            pollfd.as_mut_ptr(),
-            pollfd.len().try_into().unwrap(),
-            timeout_ms as _,
-        )
     };
+
+    let rc = unsafe { libc::poll(from_mut(pollfd).as_mut_ptr(), 1, timeout_ms as _) };
 
     if rc < 0 {
         return Err(std::io::Error::last_os_error());
